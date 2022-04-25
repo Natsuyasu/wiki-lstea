dfd039c1708b72f52eeb4a86564ff4f21f493b01
Natsuyasu <2542135846@qq.com>
2022-04-25 20:42:01 +0800

    Initial commit

---
+++ src/router/router.ts
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory, Router } from "vue-router"
+import Home from "../views/Home.vue";
+import Testing from "../views/Testing.vue";
+
+const webHistory = createWebHashHistory()
+
+export default createRouter({
+  history: webHistory,
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: () => import(/* webpackChunkName: "home" */ "../views/About.vue") },
+    { path: "/test", component: Testing },
+  ]
+})
